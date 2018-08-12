654f2b8f1894fedfda60b74980977aa3e5f1deaa
doubleaykay <the.a.cars@gmail.com>
2018-08-12 13:16:12 -0400

    add initial js code to draw maps

---
+++ maps.js
@@ -0,0 +1,36 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiZG91YmxlYXlrYXkiLCJhIjoiY2prcjFmM2sxMHZwaDNwbG1vdmI1M2p3dSJ9.lsLTFEgtfhgbI18RFypUuQ';
+
+const focoMap = new mapboxgl.Map({
+  container: 'focoMap',
+  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  center: [-72.290, 43.703],
+  zoom: 17.0
+});
+
+const collisMap = new mapboxgl.Map({
+  container: 'collisMap',
+  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  center: [-72.290, 43.703],
+  zoom: 17.0
+});
+
+const hopMap = new mapboxgl.Map({
+  container: 'hopMap',
+  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  center: [-72.290, 43.703],
+  zoom: 17.0
+});
+
+const kafMap = new mapboxgl.Map({
+  container: 'kafMap',
+  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  center: [-72.290, 43.703],
+  zoom: 17.0
+});
+
+const novackMap = new mapboxgl.Map({
+  container: 'novackMap',
+  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  center: [-72.290, 43.703],
+  zoom: 17.0
+});
