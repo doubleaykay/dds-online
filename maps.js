b5cee1c39c660014fae88d811a34bebe932f9c50
doubleaykay <the.a.cars@gmail.com>
2018-08-12 13:17:17 -0400

    update foco center

--- maps.js
+++ maps.js
@@ -3,7 +3,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZG91YmxlYXlrYXkiLCJhIjoiY2prcjFmM2sxMHZwaDNwb
 const focoMap = new mapboxgl.Map({
   container: 'focoMap',
   style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
-  center: [-72.290, 43.703],
+  center: [-72.290939, 43.703056],
   zoom: 17.0
 });
 
