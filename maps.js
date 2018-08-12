cdf2331d36d38deaa3ecccf4b1addd30a6d1904c
doubleaykay <the.a.cars@gmail.com>
2018-08-12 13:18:51 -0400

    map style and zoom as variables

--- maps.js
+++ maps.js
@@ -1,36 +1,39 @@
 mapboxgl.accessToken = 'pk.eyJ1IjoiZG91YmxlYXlrYXkiLCJhIjoiY2prcjFmM2sxMHZwaDNwbG1vdmI1M2p3dSJ9.lsLTFEgtfhgbI18RFypUuQ';
 
+var mapStyle = 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv'
+var mapZoom = 17.0
+
 const focoMap = new mapboxgl.Map({
   container: 'focoMap',
-  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  style: mapStyle,
   center: [-72.290939, 43.703056],
-  zoom: 17.0
+  zoom: mapZoom
 });
 
 const collisMap = new mapboxgl.Map({
   container: 'collisMap',
-  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  style: mapStyle,
   center: [-72.290, 43.703],
-  zoom: 17.0
+  zoom: mapZoom
 });
 
 const hopMap = new mapboxgl.Map({
   container: 'hopMap',
-  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  style: mapStyle,
   center: [-72.290, 43.703],
-  zoom: 17.0
+  zoom: mapZoom
 });
 
 const kafMap = new mapboxgl.Map({
   container: 'kafMap',
-  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  style: mapStyle,
   center: [-72.290, 43.703],
-  zoom: 17.0
+  zoom: mapZoom
 });
 
 const novackMap = new mapboxgl.Map({
   container: 'novackMap',
-  style: 'mapbox://styles/doubleaykay/cjkr3ud6nblay2rten5sb06bv',
+  style: mapStyle,
   center: [-72.290, 43.703],
-  zoom: 17.0
+  zoom: mapZoom
 });
